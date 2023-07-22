9f6483be43d23b0152533f86dc7af81026f5d2bc
Destiny Peterson <arceusthe@gmail.com>
2023-07-22 03:20:29 -0700

    Update to try to use windows-950 when extract-msg is imported (#20)
    Pulling into Dev to add tests before release

--- RTFDE/text_extraction.py
+++ RTFDE/text_extraction.py
@@ -13,6 +13,7 @@
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the included LICENSE file for details.
 
+import codecs
 import re
 from collections import namedtuple
 from typing import Union, Any, List, Tuple, Dict
@@ -197,6 +198,14 @@ Args:
 Returns:
     The name of the codec in the Python codec registry. Used as the name for enacoding/decoding.
 """
+    if codepage_num == 950:
+        # See if an implementation has been added for windows-950. If it has, use that.
+        try:
+            codecs.lookup('windows-950') # Raises LookupError if not found.
+            log.debug('Found python codec corresponding to code page {0}: windows-950'.format(codepage_num))
+            return 'windows-950'
+        except LookupError:
+            pass
     text_codec = codepages.codepage2codec(codepage_num)
     log.debug('Found python codec corresponding to code page {0}: {1}'.format(codepage_num, text_codec))
     return text_codec
