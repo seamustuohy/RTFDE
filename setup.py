ee164b8c9209a75e253d57c9656ae2d582bf4b8a
Seamus Tuohy <seamustuohy@users.noreply.github.com>
2024-06-22 11:03:09 -0400

    Updated requires to track against compatable releases. (#32)
    Updated package requirements to have more flexibility for minor updates to dependencies.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="RTFDE",
-    version="0.1.1",
+    version="0.1.2",
     author="seamus tuohy",
     author_email="code@seamustuohy.com",
     description="A library for extracting HTML content from RTF encapsulated HTML as commonly found in the exchange MSG email format.",
@@ -25,12 +25,12 @@ setuptools.setup(
         "Topic :: Communications :: Email :: Filters"
     ],
 
-    python_requires='>=3.8',
-    install_requires=['lark==1.1.8',
+    python_requires='~=3.8',
+    install_requires=['lark~=1.1.8',
                       'oletools>=0.56'],
-    extras_require={'msg_parse': ['extract_msg>=0.27'],
-                    'dev': ['lxml>=4.6',
-                            'mypy>=1.1.1',
-                            'pdoc3>=0.10.0',
-                            'coverage>=7.2.2']}
+    extras_require={'msg_parse': ['extract_msg~=0.27'],
+                    'dev': ['lxml~=4.6',
+                            'mypy~=1.1',
+                            'pdoc3~=0.10.0',
+                            'coverage~=7.2.2']}
 )
